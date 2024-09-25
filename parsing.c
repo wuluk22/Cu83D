32bd6e36b5d007ae8c208b219bfec3685ee441f1
Anne-Marie Lion <alion@shi-r5-p5.s19.be>
2024-09-25 15:31:12 +0200

    AM - fini

--- parsing.c
+++ parsing.c
@@ -6,14 +6,16 @@
 /*   By: alion <alion@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 12:51:23 by alion             #+#    #+#             */
-/*   Updated: 2024/09/25 14:42:37 by alion            ###   ########.fr       */
+/*   Updated: 2024/09/25 15:06:49 by alion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/cub3D.h"
 
-void	init(t_env *e)
+void	init(t_env *e, t_map *map)
 {
+	map->y = 0;
+	map->x_map_size = 0;
 	e->map.no = 0;
 	e->map.so = 0;
 	e->map.ea = 0;
@@ -46,7 +48,7 @@ int	parsing(int argc, char **argv, t_map *map, t_env *e)
 		ft_exit(*map, "Not a number of arguments allowed\n", 0);
 	if (check_name(argv[1]) == 1)
 		ft_exit(*map, "Not a valid name\n", 0);
-	init(e);
+	init(e, map);
 	if (get_info(e, fd) == 1)
 		ft_exit(*map, "No good info\n", 0);
 	if (parse_map(e, map, fd) != 0)
